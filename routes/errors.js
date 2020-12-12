017b2866577d730a3c4dd9c4f3206a299fbfc8b2
Alexey Belov <vaelastras@gmail.com>
2020-12-12 20:40:56 +0300

    feat: upd eslint

--- routes/errors.js
+++ routes/errors.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { NotFoundError } = require('../errors');
-const { urlNotExist} = require('../utils/error-messages/not-found-errors');
+const { urlNotExist } = require('../utils/error-messages/not-found-errors');
 
 router.use('/', (req, res, next) => {
   const err = new NotFoundError(urlNotExist);
